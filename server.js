c5352716275d1bc2d0073696e08e3de1dd455abb
aurelien-reeves-scalingo <111763415+aurelien-reeves-scalingo@users.noreply.github.com>
2023-01-30 11:23:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-  res.send(process.env)
+  res.write('Hello World!')
+  res.write(process.env)
+  res.write('This is the end of the world!')
+  res.write('(as we know it)')
+  res.end()
 })
 
 var server = app.listen(process.env.PORT || 3000, function () {
