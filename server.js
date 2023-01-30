0e55faaa3d3439d9e2d20226003714cfc8a5bbfe
aurelien-reeves-scalingo <aurelien.reeves@scalingo.com>
2023-01-30 11:13:24 +0100

    Make port configurable

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
+// Node.js - Express sample application
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
   res.send('Hello World!')
 })
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('App listening at http://%s:%s', host, port)
 })
