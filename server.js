1cce065dfb2fd36292b03c0ae2d30778d17c2ec3
aurelien-reeves-scalingo <aurelien.reeves@scalingo.com>
2023-01-30 11:33:44 +0100

    RTFM

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express()
 
 app.get('/', function (req, res) {
   res.write('Hello World!')
-  res.write(process.env.join(', '))
+  res.write(JSON.stringify(process.env))
   res.write('This is the end of the world!')
   res.write('(as we know it)')
   res.end()
