159906c8482abcfde6acbd21c5d19b3fae3b023c
aurelien-reeves-scalingo <111763415+aurelien-reeves-scalingo@users.noreply.github.com>
2023-02-21 10:48:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express()
 app.get('/', function (req, res) {
   res.write('Hello World!')
   res.write(JSON.stringify(process.env))
-  res.write("\noh no! :( That is actually really bad\n")
+  res.write("\noh no! :( That is actually really bad :(\n")
   res.write(JSON.stringify(process.env))
   res.write('This is the end of the world!')
   res.write('(as we know it)')
